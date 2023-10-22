b7add93589e29ef402d6a46ca1a7fab1e97f2804
Lucas Santos <lhs.santoss@gmail.com>
2023-10-23 01:32:11 +0200

    Remove electron and make scraper faster with one decision
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- scraper.ts
+++ scraper.ts
@@ -1,118 +1,54 @@
-const electronPkg = require("electron");
-import { request } from "obsidian";
+import { requestUrl } from 'obsidian'
 
 function blank(text: string): boolean {
-  return text === undefined || text === null || text === "";
+  return text === undefined || text === null || text === ''
 }
 
 function notBlank(text: string): boolean {
-  return !blank(text);
+  return !blank(text)
 }
 
-// async wrapper to load a url and settle on load finish or fail
-async function load(window: any, url: string): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    window.webContents.on("did-finish-load", (event: any) => resolve(event));
-    window.webContents.on("did-fail-load", (event: any) => reject(event));
-    window.loadURL(url);
-  });
-}
-
-async function electronGetPageTitle(url: string): Promise<string> {
-  const { remote } = electronPkg;
-  const { BrowserWindow } = remote;
-
+async function scrape(url: string): Promise<string> {
   try {
-    const window = new BrowserWindow({
-      width: 1000,
-      height: 600,
-      webPreferences: {
-        webSecurity: false,
-        nodeIntegration: true,
-        images: false,
-      },
-      show: false,
-    });
-    window.webContents.setAudioMuted(true);
+    const response = await requestUrl(url)
+    if (!response.headers['content-type'].includes('text/html')) return getUrlFinalSegment(url)
+    const html = response.text
 
-    await load(window, url);
+    const doc = new DOMParser().parseFromString(html, 'text/html')
+    const title = doc.querySelector('title')
 
-    try {
-      const title = window.webContents.getTitle();
-      window.destroy();
-
-      if (notBlank(title)) {
-        return title;
-      } else {
-        return url;
-      }
-    } catch (ex) {
-      window.destroy();
-      return url;
-    }
-  } catch (ex) {
-    console.error(ex);
-    return "Site Unreachable";
-  }
-}
-
-async function nonElectronGetPageTitle(url: string): Promise<string> {
-  try {
-    const html = await request({ url });
-
-    const doc = new DOMParser().parseFromString(html, "text/html");
-    const title = doc.querySelectorAll("title")[0];
-
-    if (title == null || blank(title?.innerText)) {
+    if (blank(title?.innerText)) {
       // If site is javascript based and has a no-title attribute when unloaded, use it.
-      var noTitle = title?.getAttr("no-title");
+      var noTitle = title?.getAttr('no-title')
       if (notBlank(noTitle)) {
-        return noTitle;
+        return noTitle
       }
 
       // Otherwise if the site has no title/requires javascript simply return Title Unknown
-      return url;
+      return url
     }
 
-    return title.innerText;
+    return title.innerText
   } catch (ex) {
-    console.error(ex);
-
-    return "Site Unreachable";
+    console.error(ex)
+    return 'Site Unreachable'
   }
 }
 
 function getUrlFinalSegment(url: string): string {
   try {
-    const segments = new URL(url).pathname.split('/');
-    const last = segments.pop() || segments.pop(); // Handle potential trailing slash
-    return last;
+    const segments = new URL(url).pathname.split('/')
+    const last = segments.pop() || segments.pop() // Handle potential trailing slash
+    return last
   } catch (_) {
-    return "File"
+    return 'File'
   }
 }
 
-export default async function getPageTitle(url: string): Promise<string> {
-  // If we're on Desktop use the Electron scraper
-  if (!(url.startsWith("http") || url.startsWith("https"))) {
-    url = "https://" + url;
-  }
-  const response = await fetch(url, { method: "HEAD" });
-
-  // Ensure site returns an ok status code before scraping
-  if (!response.ok) {
-    return "Site Unreachable";
+export default async function getPageTitle(url: string) {
+  if (!(url.startsWith('http') || url.startsWith('https'))) {
+    url = 'https://' + url
   }
 
-  // Ensure site is an actual HTML page and not a pdf or 3 gigabyte video file.
-  let contentType = response.headers.get("content-type");
-  if (!contentType.includes("text/html")) {
-    return getUrlFinalSegment(url);
-  }
-
-  if (electronPkg != null) {
-    return electronGetPageTitle(url);
-  } else {
-    return nonElectronGetPageTitle(url);
-  }
+  return scrape(url)
 }
